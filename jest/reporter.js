5a4fd128e1c59fb16dba51458158970d164f6299
Mario Visic <mario@mariovisic.com>
2022-05-19 09:12:49 +1000

    Use a new tracer for each test case (rather than per test file)
    Jest does not seem to have a neat way of running a hook before each test
    case, but it does have an after test case hook which we can use as a
    pseduo before-test hook, so let's do that :) This now means that we're
    correctly reporting the HTTP requests for each individual test, rather
    than previously they were being combined for a test file which meant the
    results were not correct.

--- jest/reporter.js
+++ jest/reporter.js
@@ -23,13 +23,14 @@ class JestBuildkiteAnalyticsReporter {
   }
 
   onRunStart(test) {
+    this.tracer = new Tracer()
     this.network = new Network()
     this.network.setup()
+    this.network.tracer = this.tracer
   }
 
   onRunComplete(_test, _results) {
     this.network.teardown()
-    this.tracer.finalize()
 
     if (!this._buildkiteAnalyticsToken) {
       console.error('Missing BUILDKITE_ANALYTICS_TOKEN')
@@ -74,31 +75,32 @@ class JestBuildkiteAnalyticsReporter {
     })
   }
 
-  onTestStart(test) {
-    this.tracer = new Tracer()
-    this.network.tracer = this.tracer
-  }
 
-  onTestResult(test, testResult) {
+  // NOTE: jest reporters do not seem to have a before test case hook, only a 'onTestStart' hook, which runs before each
+  // test file, but we need to setup a new tracer for each individual test, so we'll use the latter half of this
+  // onTestCaseResult as a 'before each test' hook :)
+  onTestCaseResult(test, result) {
     this.tracer.finalize()
     const testPath = this.relativeTestFilePath(testResult.testFilePath);
     const prefixedTestPath = this.prefixTestPath(testPath);
+    const id = uuidv4()
 
-    testResult.testResults.forEach((result) => {
-      let id = uuidv4()
-      this._testResults.push({
-        'id': id,
-        'scope': result.ancestorTitles.join(' '),
-        'name': result.title,
-        'identifier': result.fullName,
-        'location': result.location ? `${prefixedTestPath}:${result.location.line}` : null,
-        'file_name': prefixedTestPath,
-        'result': this.analyticsResult(result),
-        'failure_reason': this.analyticsFailureReason(result),
-        // TODO: Add support for 'failure_expanded'
-        'history': this.tracer.history(),
-      })
+    this._testResults.push({
+      'id': id,
+      'scope': result.ancestorTitles.join(' '),
+      'name': result.title,
+      'identifier': result.fullName,
+      'location': result.location ? `${prefixedTestPath}:${result.location.line}` : null,
+      'file_name': prefixedTestPath,
+      'result': this.analyticsResult(result),
+      'failure_reason': this.analyticsFailureReason(result),
+      // TODO: Add support for 'failure_expanded'
+      'history': this.tracer.history(),
     })
+
+    // Before each test case
+    this.tracer = new Tracer()
+    this.network.tracer = this.tracer
   }
 
   prefixTestPath(testFilePath) {
