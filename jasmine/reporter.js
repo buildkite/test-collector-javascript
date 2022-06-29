4dc49081745bb39d882e874fde10705412b0203e
Mario Visic <mario@mariovisic.com>
2022-06-29 18:32:15 +1000

    Record line numbers as well

--- jasmine/reporter.js
+++ jasmine/reporter.js
@@ -1,16 +1,21 @@
-let filenames = {}
+let testLocations = {}
 
 const findLocation = () => {
   const fileRegexp = /at.* \({0,1}(.*|\w*):(.*):\d*/
   const trace = (new Error()).stack.split(/\n/)
   const location = (trace.filter((line) => line.match(/spec\.js/gi))[0] || '')
+  const locationArray = location.match(fileRegexp) || []
+  return {
+    filename: locationArray[1],
+    line: locationArray[2]
+  }
   return (location.match(fileRegexp) || [])[1]
 }
 
 const itFactory = (it) => {
   return function (description, fn, timeout) {
     const spec = it.apply(this, arguments)
-    filenames[spec.id] = findLocation()
+    testLocations[spec.id] = findLocation()
     return spec
   }
 }
@@ -24,8 +29,8 @@ class JasmineBuildkiteAnalyticsReporter {
   }
 
   specDone(result) {
-    let filename = filenames[result.id]
-    // console.log('result', result)
+    result.location = testLocations[result.id]
+    console.log('result', result)
   }
 }
 
