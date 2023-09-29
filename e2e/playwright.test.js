8a47898611d94d07480b7acf974d008268ef646e
Naufan Rizal <naufan@buildkite.com>
2023-09-29 13:57:31 +1300

    test: add e2e test for Playwright

---
+++ e2e/playwright.test.js
@@ -0,0 +1,105 @@
+// Does an end-to-end test of the Playwright example, using the debug output from the
+// reporter, and verifying the JSON
+require('dotenv').config();
+const { exec, spawn } = require('child_process');
+const path = require('path');
+
+const TIMEOUT = 10000;
+
+const runPlaywright = (args, env) => {
+  const cwd = path.join(__dirname, "../examples/playwright");
+
+  return new Promise((resolve) => {
+    const command = `npx playwright test ${args.join(' ')}`
+    exec(command, { cwd, env: { ...env, JEST_WORKER_ID: undefined } }, (error, stdout) => {
+      resolve(stdout)
+    })
+  })
+}
+
+describe('examples/playwright', () => {
+  const env = {
+    ...process.env,
+    BUILDKITE_ANALYTICS_TOKEN: "xyz",
+    BUILDKITE_ANALYTICS_DEBUG_ENABLED: "true"
+  }
+
+  describe('when token is defined through reporter options', () => {
+    test('it uses the correct token', async () => {
+      const stdout = await runPlaywright(["--config", "token.config.js"], { ...env, BUILDKITE_ANALYTICS_PLAYWRIGHT_TOKEN: 'abc' })
+
+      expect(stdout).toMatch(/.*Test Analytics Sending: ({.*})/m);
+      const jsonMatch = stdout.match(/.*Test Analytics Sending: ({.*})/m)
+      const headers = JSON.parse(jsonMatch[1])["headers"]
+
+      expect(headers).toHaveProperty("Authorization", 'Token token="abc"')
+    }, TIMEOUT)
+  });
+
+  describe('when token is defined through BUILDKITE_ANALYTICS_TOKEN', () => {
+    test('it uses the correct token', async () => {
+      const stdout = await runPlaywright([], env)
+
+      expect(stdout).toMatch(/.*Test Analytics Sending: ({.*})/m);
+      const jsonMatch = stdout.match(/.*Test Analytics Sending: ({.*})/m)
+      const headers = JSON.parse(jsonMatch[1])["headers"]
+
+      expect(headers).toHaveProperty("Authorization", 'Token token="xyz"')
+    }, TIMEOUT)
+  });
+
+  test('it posts the correct JSON', async () => {
+    const stdout = await runPlaywright([], env)
+
+    expect(stdout).toMatch(/.*Test Analytics Sending: ({.*})/m);
+    const jsonMatch = stdout.match(/.*Test Analytics Sending: ({.*})/m)
+    const data = JSON.parse(jsonMatch[1])["data"]
+
+    expect(data).toHaveProperty("format", "json")
+
+    expect(data).toHaveProperty("run_env.ci")
+    expect(data).toHaveProperty("run_env.debug", 'true')
+    expect(data).toHaveProperty("run_env.key")
+    expect(data).toHaveProperty("run_env.version")
+    expect(data).toHaveProperty("run_env.collector", "js-buildkite-test-collector")
+
+    expect(data).toHaveProperty("data[0].scope", ' chromium example.spec.js has title')
+    expect(data).toHaveProperty("data[0].name", 'has title')
+    expect(data).toHaveProperty("data[0].location", "tests/example.spec.js:3:1")
+    expect(data).toHaveProperty("data[0].file_name", "tests/example.spec.js")
+    expect(data).toHaveProperty("data[0].result", 'passed')
+
+    expect(data).toHaveProperty("data[1].scope", " chromium example.spec.js says hello")
+    expect(data).toHaveProperty("data[1].name", "says hello")
+    expect(data).toHaveProperty("data[1].location", "tests/example.spec.js:9:1")
+    expect(data).toHaveProperty("data[1].file_name", "tests/example.spec.js")
+    expect(data).toHaveProperty("data[1].result", "failed")
+    expect(data).toHaveProperty("data[1].failure_reason", "Error: Timed out 5000ms waiting for expect(received).toHaveText(expected)")
+    expect(data).toHaveProperty("data[1].failure_expanded", expect.arrayContaining([
+      expect.objectContaining({
+        expanded: expect.arrayContaining(['Expected string: "Hello, World!"', 'Received string: "¡Hola Mundo!"'])
+      })
+    ]))
+  }, TIMEOUT);
+
+  test('it posts all executions when failed tests are retried', async () => {
+    const stdout = await runPlaywright(["--retries=1"], env)
+
+    const jsonMatch = stdout.match(/.*Test Analytics Sending: ({.*})/m)
+    const data = JSON.parse(jsonMatch[1])["data"]["data"];
+    expect(data.length).toEqual(3)
+  }, 2 * TIMEOUT);
+
+  test('it supports test location prefixes for monorepos', async () => {
+    const stdout = await runPlaywright([], { ...env, BUILDKITE_ANALYTICS_LOCATION_PREFIX: "some-sub-dir/" })
+
+    expect(stdout).toMatch(/.*Test Analytics Sending: ({.*})/m);
+    const jsonMatch = stdout.match(/.*Test Analytics Sending: ({.*})/m)
+    const data = JSON.parse(jsonMatch[1])["data"]
+
+    expect(data).toHaveProperty("run_env.location_prefix", "some-sub-dir/")
+
+    expect(data).toHaveProperty("data[0].location", "some-sub-dir/tests/example.spec.js:3:1")
+    expect(data).toHaveProperty("data[1].location", "some-sub-dir/tests/example.spec.js:9:1")
+  }, TIMEOUT);
+});
