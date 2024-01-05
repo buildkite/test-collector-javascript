07a6ecc1c9df548dcdf8ffbb68efe88a4d97b653
Naufan Rizal <naufan@buildkite.com>
2024-01-05 17:52:10 +1100

    feat: Implement vitest reporter

---
+++ vitest/reporter.js
@@ -0,0 +1,8 @@
+class VitestBuildkiteAnalyticsReporter {
+  onFinished(files) {
+    // TODO: send results to Test Analytics
+    console.log(files)
+  }
+}
+
+module.exports = VitestBuildkiteAnalyticsReporter
