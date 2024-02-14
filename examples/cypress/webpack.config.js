06d4b1597da0276fcd1f6623b6e69b3649f8f91e
Naufan Rizal <naufan@buildkite.com>
2024-02-14 13:06:31 +1100

    fix formatting

--- examples/cypress/webpack.config.js
+++ examples/cypress/webpack.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-	module: {
-		rules: [
-			{
-				test: /\.(jsx|js)$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env', '@babel/preset-react'],
-					},
-				},
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 };
