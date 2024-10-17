768d5f3650fee6ebdb909f427277eb7ec905b03c
Naufan Rizal <naufan@buildkite.com>
2024-10-17 15:56:29 +1100

    Log response error even when debug mode is disabled

--- util/uploadTestResults.js
+++ util/uploadTestResults.js
@@ -37,22 +37,22 @@ const uploadTestResults = (env, results, options, done) => {
       // Do something with request error
       return Promise.reject(error);
     });
-
-    // Add a response interceptor
-    axios.interceptors.response.use(function (response) {
-      // Any status code that lie within the range of 2xx cause this function to trigger
-      // Do something with response data
-      Debug.log(`Test Analytics success response ${JSON.stringify(response.data)}`);
-      return response;
-    }, function (error) {
-      if (error.response) {
-        Debug.log(`Test Analytics error response: ${error.response.status} ${error.response.statusText} ${JSON.stringify(error.response.data)}`);
-      } else {
-        Debug.log(`Test Analytics error: ${error.message}`)
-      }
-      return Promise.reject(error);
-    });
   }
+
+  // Add a response interceptor
+  axios.interceptors.response.use(function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    Debug.log(`Test Analytics success response ${JSON.stringify(response.data)}`);
+    return response;
+  }, function (error) {
+    if (error.response) {
+      console.log(`⚠️ Test Analytics error response: ${error.response.status} ${error.response.statusText} ${JSON.stringify(error.response.data)}`);
+    } else {
+      console.log(`⚠️ Test Analytics error: ${error.message}`)
+    }
+    return Promise.reject(error);
+  });
 
   for (let i = 0; i < results.length; i += CHUNK_SIZE) {
     const data = {
